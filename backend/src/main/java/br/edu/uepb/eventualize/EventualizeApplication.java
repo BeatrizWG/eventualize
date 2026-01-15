39ae06ec527e5f1d846eb721a5937f5b58c7ac14
Beatriz <beatriz.wg@hotmail.com>
2026-01-15 20:45:46 -0300

    chore(backend): inicializa estrutura do projeto e arquivos base

---
+++ backend/src/main/java/br/edu/uepb/eventualize/EventualizeApplication.java
@@ -0,0 +1,13 @@
+package br.edu.uepb.eventualize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventualizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventualizeApplication.class, args);
+	}
+
+}
